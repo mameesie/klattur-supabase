e679fb33de59c9da5e71aeb54a696715385f2e04
Mees <meesboers@hotmail.com>
2025-10-18 19:04:49 +0200

    small screens side pannel overlaps

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -10,27 +10,31 @@ function ChatPage() {
 
 
   return (
-    <div className="flex h-full">
-      {sidePanelOut ? (<div className="flex-col w-[300px] flex-shrink-0 bg-gray-200 ">
+  <div className="flex h-full">
+    {sidePanelOut ? (
+      <div className="absolute md:relative md:flex-col w-[300px] md:w-[300px] h-screen md:h-auto flex-shrink-0 bg-gray-200 z-50">
         <div className="flex justify-end p-[15px]">
           <button 
-          onClick={() => setSidePanelOut(!sidePanelOut)}
-          className=" w-[20px] overflow-hidden">
+            onClick={() => setSidePanelOut(!sidePanelOut)}
+            className="w-[20px] overflow-hidden">
             <SidePanel />
           </button>
         </div>
-      </div>) : ( <div className="flex-col w-[50px] flex-shrink-0 bg-gray-200">
+      </div>
+    ) : (
+      <div className="flex-col w-[50px] h-screen md:h-auto flex-shrink-0 bg-gray-200 z-50">
         <div className="flex justify-center p-[15px]">
           <button
             onClick={() => setSidePanelOut(!sidePanelOut)}
-            className=" w-[20px] overflow-hidden">
-              <SidePanel />
-            </button>
+            className="w-[20px] overflow-hidden">
+            <SidePanel />
+          </button>
         </div>
-      </div>)}
-      <ChatBox />
-    </div>
-  );
+      </div>
+    )}
+    <ChatBox />
+  </div>
+);
 }
 
 export default ChatPage;
