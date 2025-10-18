f92b8b1396cd1c381bb2a7530f448486a3751ee9
Mees <meesboers@hotmail.com>
2025-10-18 19:07:22 +0200

    on small screen side pannel button is floating in the left corner

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -22,7 +22,7 @@ function ChatPage() {
         </div>
       </div>
     ) : (
-      <div className="flex-col w-[50px] h-screen md:h-auto flex-shrink-0 bg-gray-200 z-50">
+      <div className="absolute md:relative md:flex-col w-[50px] flex-shrink-0 bg-gray-200 z-50">
         <div className="flex justify-center p-[15px]">
           <button
             onClick={() => setSidePanelOut(!sidePanelOut)}
