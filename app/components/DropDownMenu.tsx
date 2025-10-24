9c93e5ac71f5e7f9d37879572eb95ddaea0dff9b
Mees <meesboers@hotmail.com>
2025-10-24 17:42:17 +0200

    changed drop down menu icon

--- app/components/DropDownMenu.tsx
+++ app/components/DropDownMenu.tsx
@@ -2,30 +2,28 @@ import { DropdownMenu } from 'radix-ui'
 import React from 'react'
 import { changeEmail } from '../actions/actions'
 import LogOutButton from './LogOutButton'
+import ChatUser from '@/public/svg/chatUser';
 
 interface UserData {
-  user_metadata: {
-    avatar_url: string;
-    first_name: string;
-    name: string;
-  };
+  avatar_url: string;
+  first_name: string;
+  
+  
 }
 
-interface DropDownMenuProps {
-  user: UserData;
-}
 
-function DropDownMenu({ user }: DropDownMenuProps) {
+
+function DropDownMenu({ avatar_url, first_name }: UserData) {
   return (
     <DropdownMenu.Root>
               <DropdownMenu.Trigger asChild>
-                <button>
-                  <img
-                    className="h-[40px] w-[40px]"
-                    alt="Profile picture"
-                    src={user?.user_metadata?.avatar_url}
-                  ></img>
-                </button>
+                <div className='mr-[20px]'>
+                  <button className='bg-pink-light w-[60px] h-[60px] rounded-full flex items-center justify-center cursor-pointer'>
+                    <div className='w-[40px] h-[40px]'>
+                      <ChatUser/>
+                    </div>
+                  </button>
+                </div>
               </DropdownMenu.Trigger>
               <DropdownMenu.Portal>
                 <DropdownMenu.Content className="z-50 min-w-[220px] min-h-[400px] rounded-md bg-white p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
@@ -37,7 +35,7 @@ function DropDownMenu({ user }: DropDownMenuProps) {
                     <LogOutButton />
                   </DropdownMenu.Item>
                  <DropdownMenu.Item>
-                    <a href="/profile">{user?.user_metadata?.first_name ?? user?.user_metadata?.name}</a>
+                    <a href="/profile">{first_name}</a>
                   </DropdownMenu.Item>
                   <DropdownMenu.Item>
                     <a href="/change-password" >Verander wachtwoord</a>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,26 +1,31 @@
 import { getUser } from "@/supabase/auth/server";
 import LogOutButton from "./LogOutButton";
 import DropDownMenu from "./DropDownMenu";
+
+import KlatturLogo from "@/public/svg//KlatturLogo";
 async function Header() {
   const user = await getUser();
 
-  console.log("user: ",user);
+  console.log("user: ", user);
   return (
-    <div className="h-[100px] bg-white sticky top-0 flex-shrink-0 z-20">
-       <div className="flex">
-            <a href="/chat">Logo</a>
-            <p>{user?.user_metadata?.first_name ?? user?.user_metadata?.name}</p>
-            <LogOutButton />
-          </div>
-      {user ? (
-        <DropDownMenu user={{user_metadata: {avatar_url: user.user_metadata.avatar_url, first_name: user.user_metadata.first_name, name: user.user_metadata.name}}}></DropDownMenu>
+    <div className="h-[100px] bg-white sticky flex justify-between items-center top-0 flex-shrink-0 z-20">
       
-      ) : (
-        <>
-          <a href="/login">Login</a>
-          <a href="/signup">Sign up</a>
-        </>
-      )}
+        <a className="w-[100px] ml-[10px]" href="/chat"><KlatturLogo/></a>
+
+        {user ? (
+          <DropDownMenu
+            avatar_url={user?.user_metadata?.avatar_url}
+            first_name={
+              user?.user_metadata?.first_name ?? user?.user_metadata?.name
+            }
+          ></DropDownMenu>
+        ) : (
+          <>
+            <a href="/login">Login</a>
+            <a href="/signup">Sign up</a>
+          </>
+        )}
+      
     </div>
   );
 }
